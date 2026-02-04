22c3b53f7d5b1e6463731ba3fb9e51f76eddc8c5
Teck-belgium <skyteck.belgium@gmail.com>
2026-02-04 19:15:04 +0100

    Create config.js

---
+++ includes/js/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3cseAFVuHekCzRBltFrZYbD-nBUwAQqU",
+  authDomain: "teck-2c7cc.firebaseapp.com",
+  projectId: "teck-2c7cc",
+  storageBucket: "teck-2c7cc.firebasestorage.app",
+  messagingSenderId: "817998080512",
+  appId: "1:817998080512:web:ac038ab80849dbbb21c6e5"
+};
