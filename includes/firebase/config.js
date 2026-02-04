c8167f99a3e12409f0cd61e07d8abd6b910acf5a
Teck-belgium <skyteck.belgium@gmail.com>
2026-02-04 19:23:54 +0100

    Update config.js

--- includes/firebase/config.js
+++ includes/firebase/config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD3cseAFVuHekCzRBltFrZYbD-nBUwAQqU",
   authDomain: "teck-2c7cc.firebaseapp.com",
@@ -6,3 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "817998080512",
   appId: "1:817998080512:web:ac038ab80849dbbb21c6e5"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
