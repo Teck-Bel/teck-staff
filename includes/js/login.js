85b20fa0ec437833896b762861eda15a4cf069d7
Teck-belgium <skyteck.belgium@gmail.com>
2026-02-04 19:22:33 +0100

    Update login.js

--- includes/js/login.js
+++ includes/js/login.js
@@ -1,4 +1,4 @@
-import { auth } from "./config.js";
+import { auth } from "./teck-staff/includes/firebase/config.js";
 import { signInWithEmailAndPassword } from 
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
