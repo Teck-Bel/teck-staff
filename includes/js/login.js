ea9dcb367f3d72a341de9f51ba45db2686eb3d5e
Teck-belgium <skyteck.belgium@gmail.com>
2026-02-05 18:35:39 +0100

    Update login.js

--- includes/js/login.js
+++ includes/js/login.js
@@ -2,19 +2,22 @@ import { auth } from "/teck-staff/includes/firebase/config.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const loginBtn = document.getElementById("loginBtn");
-  if (!loginBtn) return;
+  const form = document.getElementById("loginForm");
+  const errorE1 = document.getElementById("error");
+  
+  if (!form) return;
 
-  loginBtn.addEventListener("click", async () => {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const errorEl = document.getElementById("error");
 
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      await singInWithEmailAndPassword(auth, email, password);
       window.location.href = "/teck-staff/dashboard.html";
     } catch (err) {
-      errorEl.textContent = "Ongeldige inloggegevens";
+      errorE1.textContent = "Ongeldige inloggegevens";
       console.error(err.code);
     }
   });
